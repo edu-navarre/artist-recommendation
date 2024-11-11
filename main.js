c13d98f02d6cc21ce32a86ddee486b5c7f6d8edc
Edu Navarre <eduardo.navarre.montero@gmail.com>
2024-11-11 16:09:12 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+/* 
+1. This program will take an musical instrument as an input.
+2. The valid options are: Drums, Bass, Guitar, Piano, Vocals, Saxophone, Trumpet or Trombone.
+3. Depending on the user input (instrument), the program will output a random name (of 10) of
+a recommended artist to listen to.
+
+For example:
+User Input: Drums
+Output: "We recommend you listen to Dennis Chambers"
+
+Or
+
+User Input: Basson
+Output: "Please enter a valid instrument"
+*/
